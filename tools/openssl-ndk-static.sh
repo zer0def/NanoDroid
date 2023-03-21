c8975c47749bfe530204e9890c8d29e36d52fccc
zer0def <zer0def@zer0def.0>
2023-03-21 09:39:19 +0100

    OpenSSL bump to 3.2.0

--- tools/openssl-ndk-static.sh
+++ tools/openssl-ndk-static.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-OPENSSL_VERSION="${OPENSSL_VERSION:-1.1.1q}"
+OPENSSL_VERSION="${OPENSSL_VERSION:-3.2.0}"
 curl -sSL "https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz" | tar xz
 cd "openssl-${OPENSSL_VERSION}"
 sed -i 's/-mandroid//' Configurations/15-android.conf
 
-export CXXFLAGS="-std=c++11 -fPIC" CPPFLAGS="-DANDROID -fPIC"
-export ANDROID_NDK_HOME="${ANDROID_NDK_HOME:-/opt/android-sdk/ndk/24.0.8215888}"
+export CXXFLAGS="-std=c++11 -fPIC" CPPFLAGS="-DANDROID -fPIC" ANDROID_NDK="${ANDROID_NDK:-/opt/android-sdk/ndk/26.1.10909125}"
+export ANDROID_NDK_HOME="${ANDROID_NDK}" ANDROID_NDK_ROOT="${ANDROID_NDK}" ANDROID_NDK_LATEST_HOME="${ANDROID_NDK}"
 
 build_android_clang(){  # assumes arg3 is always gcc, which is kind of wrong
   OPENSSL_PLATFORM="android-${1}" ANDROID_API="${2}" TOOLCHAIN="${3:-llvm}"  #TOOLCHAIN="${3:+${3}${2}}"  # llvm
@@ -27,10 +27,15 @@ build_android_clang(){  # assumes arg3 is always gcc, which is kind of wrong
          SYSROOT="${TOOLCHAIN_ROOT}/sysroot" \
          PATH="${TOOLCHAIN_ROOT}/bin${PATH:+:${PATH}}"
 
+  OPENSSL3_OPTS="no-tests"  # ??, "duplicate symbol: time" when building tests
   make clean
-  ./Configure --static -static -ffunction-sections -fdata-sections -Wl,--gc-sections "${OPENSSL_PLATFORM}" no-shared threads no-asm no-sse2 ${ANDROID_API:+-D__ANDROID_API__=${ANDROID_API}}  #no-hw no-engine
+  ./Configure --static -static -ffunction-sections -fdata-sections -Wl,--gc-sections "${OPENSSL_PLATFORM}" \
+    no-shared threads no-asm no-sse2 no-ssl3-method no-ssl3 no-weak-ssl-ciphers no-ssl no-deprecated no-tls1 \
+    no-tls1-method no-tls1_1 no-tls1_1-method no-tls1_2 no-tls1_2-method enable-tls1_3 no-rc2 no-rc4 no-idea no-seed \
+    ${OPENSSL3_OPTS} ${ANDROID_API:+-D__ANDROID_API__=${ANDROID_API}}  #no-hw no-engine
   make -j${NPROC:-$(nproc)} build_generated
-  make -j${NPROC:-$(nproc)} apps
+  #make -j${NPROC:-$(nproc)} apps  # ossl1.1
+  make -j${NPROC:-$(nproc)} build_programs
   "${STRIP}" apps/openssl
 }
 
